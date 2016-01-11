d1c620b3a9775f216037140f78f9f8ccb196111d
hemanths <hemanths@Administrators-MacBook-Pro.local>
2016-01-11 11:33:03 +0530

    Santhosh, Hemanth | changed url of get events.

--- event-log-service-webapp/src/main/java/org/bahmni/module/eventlogservice/controller/EventLogController.java
+++ event-log-service-webapp/src/main/java/org/bahmni/module/eventlogservice/controller/EventLogController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/eventlogservice")
+@RequestMapping("/rest/eventlog")
 public class EventLogController {
     private EventLogRepository eventLogRepository;
 
