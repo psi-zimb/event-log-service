a2eaefb932edf9dc1f2fe3b36b864bc8bd7223ae
Shruthip <shruthi.pitta@gmail.com>
2016-08-18 18:52:05 +0530

    Shruthi|Fix for integration tests
    Remove few truncate table for person,person_attribute as they are no more referred
    to in event log service

--- event-log-service-webapp/src/test/resources/testDataSet/truncateTables.sql
+++ event-log-service-webapp/src/test/resources/testDataSet/truncateTables.sql
@@ -1,7 +1,2 @@
 truncate TABLE event_log;
 truncate TABLE event_records;
-truncate TABLE person_attribute;
-truncate TABLE person_attribute_type;
-truncate TABLE encounter;
-truncate TABLE person;
-truncate TABLE address_hierarchy_entry
